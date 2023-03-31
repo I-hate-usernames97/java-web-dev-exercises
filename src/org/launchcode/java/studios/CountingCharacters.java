bc39ea02a7f9bce257aec3b5e8a5e25dc7259ff5
chrishahn <chrishahn97@outlookcom>
2023-03-30 20:19:04 -0500

    Studio: Control Flow and Collections done

--- src/org/launchcode/java/studios/CountingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters.java
@@ -1,4 +1,4 @@
-package org.launchcode.java.studios.areaofacircle;
+package org.launchcode.java.studios;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,19 +12,13 @@ public class CountingCharacters {
         System.out.println("Please enter a phrase");
         String myString = input.nextLine();
 //        String myString = ("If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.");
-        char[] charactersInString = myString.toLowerCase().toCharArray();
 
-        for ( char i : charactersInString){
-
-            int num = 0;
-
-            for (char c : charactersInString) {
-
-                if (i == c) {
-                    num += 1;
-                }
-            }
-            total.put(i, num);
+        for ( char i : myString.toLowerCase().toCharArray()){
+          if (total.containsKey(i)){
+              total.put(i, total.get(i) + 1);
+          } else {
+              total.put(i, 1);
+          }
         }
 
         for (Map.Entry<Character, Integer> characters : total.entrySet()) {
